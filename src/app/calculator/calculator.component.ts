b3d5609e393e2117cd918e51db619028394ca5fa
zuikov <andrei_z@ukr.net>
2018-03-27 17:22:13 +0300

    types were added for some variables

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -10,16 +10,16 @@ export class CalculatorComponent implements OnInit {
 
   ngOnInit() {
 
-    function add(a, b) { return a + b; }
-    function subtraction(a, b) { return a - b; }
-    function multiplication(a, b) { return a * b; }
-    function division(a, b) { return a / b; }
+    function add(a: number, b: number) { return a + b; }
+    function subtraction(a: number, b: number) { return a - b; }
+    function multiplication(a: number, b: number) { return +(a * b); }
+    function division(a: number, b: number) { return +(a / b); }
 
     class Command {
-      execute: any;
-      undo: any;
-      value: any;
-      constructor(execute: any, undo: any, value: any) {
+      execute: Function;
+      undo: Function;
+      value: number;
+      constructor(execute: Function, undo: Function, value: number) {
         this.execute = execute;
         this.undo = undo;
         this.value = value;
@@ -27,33 +27,33 @@ export class CalculatorComponent implements OnInit {
     }
 
     class AddCommand {
-      constructor(value) {
+      constructor(value: number) {
         return new Command(add, subtraction, value);
       }
     }
 
     class SubtractionCommand {
-      constructor(value) {
+      constructor(value: number) {
         return new Command(subtraction, add, value);
       }
     }
 
     class MultiplicationCommand {
-      constructor(value) {
+      constructor(value: number) {
         return new Command(multiplication, division, value);
       }
     }
 
     class DivisionCommand {
-      constructor(value) {
+      constructor(value: number) {
         return new Command(division, multiplication, value);
       }
     }
 
     class Calculator {
-      _current: any;
-      _commands: any;
-      _currentSum: any;
+      _current: number;
+      _commands: string[];
+      _currentSum: number;
 
       constructor() {
         this._current = 0;
@@ -80,27 +80,27 @@ export class CalculatorComponent implements OnInit {
 
 
     // Присваиваем узлы DOM в javascript-переменные  
-    var input = document.getElementById('input'), // input/output button
+    const input = document.getElementById('input'), // input/output button
       number = document.querySelectorAll('.numbers div'), // number buttons
       operator = document.querySelectorAll('.operators div'), // operator buttons
       result = document.getElementById('result'), // equal button
       undo = document.getElementById('undo'), // undo button
-      clear = document.getElementById('clear'), // clear button
-      resultDisplayed = false; // flag to keep an eye on what output is displayed
+      clear = document.getElementById('clear'); // clear button
+    let resultDisplayed = false; // flag to keep an eye on what output is displayed
 
-    var undoString = [];
+    let undoString = [];
 
     //-------------------------------------------------------------------------------------------------------
     // здесь логика ввода чисел
     // adding click handlers to number buttons
-    for (var i = 0; i < number.length; i++) {
+    for (let i = 0; i < number.length; i++) {
       number[i].addEventListener("click", function (event) {
 
         // storing current input string and its last character in variables - used later
-        var currentString = input.innerHTML;
-        var lastChar = currentString[currentString.length - 1];
+        let currentString = input.innerHTML;
+        let lastChar = currentString[currentString.length - 1];
 
-        var currentEvent = <Element>event.target;
+        let currentEvent = <Element>event.target;
 
         // if result is not diplayed, just keep adding
         if (resultDisplayed === false) {
@@ -127,21 +127,21 @@ export class CalculatorComponent implements OnInit {
     //-------------------------------------------------------------------------------------------------------
     // здесь логика ввода операторов
     // adding click handlers to operator buttons
-    for (var i = 0; i < operator.length; i++) {
+    for (let i = 0; i < operator.length; i++) {
       operator[i].addEventListener("click", function (event) {
 
         // storing current input string and its last character in variables - used later
-        var currentString = input.innerHTML;
-        var lastChar = currentString[currentString.length - 1];
+        let currentString = input.innerHTML;
+        let lastChar = currentString[currentString.length - 1];
 
-        var currentEvent = <Element>event.target;
+        let currentEvent = <Element>event.target;
 
         // if last character entered is an operator, replace it with the currently pressed one
         if (lastChar === "+" ||
           lastChar === "-" ||
           lastChar === "×" ||
           lastChar === "÷") {
-          var newString = currentString.substring(0, currentString.length - 1) + currentEvent.innerHTML;
+          let newString = currentString.substring(0, currentString.length - 1) + currentEvent.innerHTML;
           input.innerHTML = newString;
         } else if (currentString.length == 0) {
           // if first key pressed is an opearator, don't do anything
@@ -170,61 +170,66 @@ export class CalculatorComponent implements OnInit {
     result.addEventListener("click", function () {
 
       // this is the string that we will be processing eg. -10+26+33-56*34/23
-      var inputString = input.innerHTML;
+      let inputString = input.innerHTML;
 
       undoString.push(inputString);
 
       // forming an array of numbers. eg for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
-      var numbers = inputString.split(/\+|\-|\×|\÷/g);
+      let numbers: string[] = inputString.split(/\+|\-|\×|\÷/g);
+
+      // let numbers = numbersString.map((item) => {
+      //   parseFloat(item);
+      // });
+
 
       // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
       // first we replace all the numbers and dot with empty string and then split
-      var operators = inputString.replace(/[0-9]|\./g, "").split("");
+      let operators = inputString.replace(/[0-9]|\./g, "").split("");
 
       // now we are looping through the array and doing one operation at a time.
       // first divide, then multiply, then subtraction and then addition
       // as we move we are alterning the original numbers and operators array
       // the final element remaining in the array will be the output
 
-      var divide = operators.indexOf("÷");
+      let divide = operators.indexOf("÷");
       while (divide != -1) {
-        instance.execute(new AddCommand(numbers[divide]));
-        instance.execute(new DivisionCommand(numbers[divide + 1]));
-        numbers.splice(divide, 2, instance.getCurrentValue());
+        instance.execute(new AddCommand(parseFloat(numbers[divide])));
+        instance.execute(new DivisionCommand(parseFloat(numbers[divide + 1])));
+        numbers.splice(divide, 2, instance.getCurrentValue().toString());
         operators.splice(divide, 1);
         divide = operators.indexOf("÷");
         instance._currentSum += instance._current;
         instance._current = 0;
       }
 
-      var multiply = operators.indexOf("×");
+      let multiply = operators.indexOf("×");
       while (multiply != -1) {
-        instance.execute(new AddCommand(numbers[multiply]));
-        instance.execute(new MultiplicationCommand(numbers[multiply + 1]));
-        numbers.splice(multiply, 2, instance.getCurrentValue());
+        instance.execute(new AddCommand(parseFloat(numbers[multiply])));
+        instance.execute(new MultiplicationCommand(parseFloat(numbers[multiply + 1])));
+        numbers.splice(multiply, 2, instance.getCurrentValue().toString());
         operators.splice(multiply, 1);
         multiply = operators.indexOf("×");
         instance._currentSum += instance._current;
         instance._current = 0;
       }
 
-      var subtract = operators.indexOf("-");
+      let subtract = operators.indexOf("-");
       while (subtract != -1) {
-        instance.execute(new AddCommand(numbers[subtract]));
-        instance.execute(new SubtractionCommand(numbers[subtract + 1]));
-        numbers.splice(subtract, 2, instance.getCurrentValue());
+        instance.execute(new AddCommand(parseFloat(numbers[subtract])));
+        instance.execute(new SubtractionCommand(parseFloat(numbers[subtract + 1])));
+        numbers.splice(subtract, 2, instance.getCurrentValue().toString());
         operators.splice(subtract, 1);
         subtract = operators.indexOf("-");
         instance._currentSum += instance._current;
         instance._current = 0;
       }
 
-      var add = operators.indexOf("+");
+      let add = operators.indexOf("+");
       while (add != -1) {
         // using parseFloat is necessary, otherwise it will result in string concatenation :)
         instance.execute(new AddCommand(parseFloat(numbers[add])));
         instance.execute(new AddCommand(parseFloat(numbers[add + 1])));
-        numbers.splice(add, 2, instance.getCurrentValue());
+        numbers.splice(add, 2, instance.getCurrentValue().toString());
         operators.splice(add, 1);
         add = operators.indexOf("+");
         instance._currentSum += instance._current;
